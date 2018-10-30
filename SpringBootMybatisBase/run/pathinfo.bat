10ec557030a3bb0d74f992799961373e62ee19aa
X_TryAgain <2637551443@>
2018-10-30 15:17:29 +0800

    java外部运行脚本

---
+++ SpringBootMybatisBase/run/pathinfo.bat
@@ -0,0 +1,5 @@
+set JAVA_HOME=D:\MyDevelop\jdk8\
+set GRADLE_HOME=D:\MyDevelop\gradle-4.10.2\
+set GRADLE_USER_HOME=D:\MyDevelop\gradle-user-home\
+set PATH=%JAVA_HOME%bin;%PATH%
+set PATH=%GRADLE_HOME%bin;%PATH%
